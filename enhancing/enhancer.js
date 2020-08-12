aedf8bacf6034e36bec6cb86a70688901604fdc7
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2020-08-11 19:09:53 -0700

    functions are working correctly, mvp finished

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,26 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    item.enhancement += 1;
+  }
+  return item;
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    item.durability -= 5
+  } else if (item.enhancement > 15) {
+    item.durability -= 10
+    item.enhancement -= 1
+  } else {
+    item.durability -= 10
+  }
+  return {...item}
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,27 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('game functions', () => {
+    const item = { name: 'Cram Opener', durability: 25, enhancement: 16}
+
+    describe('repair item test', () => {
+        test('return item object with durability restored', () => {
+            const repairedItem = {...item, durability: 100}
+            expect(enhancer.repair(item)).toEqual(repairedItem)
+        })
+    });
+
+    describe('success item test', () => {
+        test('item succesfully modified', () => {
+            const modifiedItem = {...item, enhancement: 17}
+            expect(enhancer.success(item)).toEqual(modifiedItem)
+        })
+    });
+
+    describe('fail item test', () => {
+        test('failed to modify item', () => {
+            const modifiedItem = {...item, durability: 15, enhancement: 16}
+            expect(enhancer.fail(item)).toEqual(modifiedItem);
+        })
+    })
+})
